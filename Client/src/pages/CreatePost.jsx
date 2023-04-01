3319c70d06e43fd2fae99c6962ff75a9212200dc
Yuvraj <yuvrajsigh1402@gmail.com>
2023-04-01 18:31:23 +0530

    FormField layout

--- Client/src/pages/CreatePost.jsx
+++ Client/src/pages/CreatePost.jsx
@@ -15,9 +15,50 @@ const CreatePost = () => {
   //While we make contact with API and wait for images
   const [generatingimg, setGeneratingimg] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
   return (
     <section className='max-w-7xl mx-auto'>
-      <div></div>
+      <div>
+            <h1 className='font-extrabold text-[#222328] text-[32px]' >
+                Create
+            </h1>
+            <p className='mt-2 text-[#666e75] text-[16px] max-w [500px]'>Create imaginative and visually stunning images gthrough DALL-E AI and share them with the community</p>
+        </div>
+
+        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-5'>
+            <FormField
+              LabelName='Your name'
+              type='text'
+              name='name'
+              placeholder='John Doe'
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField
+              LabelName='Prompt'
+              type='text'
+              name='prompt'
+              placeholder='A plush toy robot sitting against a yellow wall'
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+          </div>
+        </form>
     </section>
   )
 }
