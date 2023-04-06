b3a5401e6a0fbf1941122148549664243ea73af2
Yuvraj <yuvrajsigh1402@gmail.com>
2023-04-06 19:15:59 +0530

    sharewithcommunity-page

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_CLOUD_NAME,
-    api_aecret: process.env.CLOUDINARY_CLOUD_NAME,
+    api_secret: process.env.CLOUDINARY_CLOUD_NAME,
 })
 
 export default router;
